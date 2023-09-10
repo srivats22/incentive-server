c9158e1bdb1a97c701ccf284ed846b62365cf4a6
Srivats Venkataraman <Srivats.venkataraman@gmail.com>
2023-09-10 14:06:27 -0400

    Reverted back to file system

--- index.js
+++ index.js
@@ -6,24 +6,9 @@ const app = express();
 require('dotenv').config()
 
 var admin = require("firebase-admin");
-var serviceJson = {
-    "type": process.env.REACT_TYPE,
-    "project_id": process.env.REACT_PROJECTID,
-    "private_key_id": process.env.REACT_PRIVATE_KEY_ID,
-    "private_key": process.env.REACT_PRIVATE_KEY,
-    "client_email": process.env.REACT_CLIENT_EMAIL,
-    "client_id": process.env.REACT_CLIENT_ID,
-    "auth_uri": process.env.REACT_AUTH_URI,
-    "token_uri": process.env.REACT_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.REACT_AUTH_PROVIDER_x509_CERT_URL,
-    "client_x509_cert_url": process.env.REACT_CLIENT_X509_CERT_URL,
-    "universe_domain": process.env.UNIVERSE_DOMAIN
-}
-
-console.log(process.env.REACT_PRIVATE_KEY_ID)
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceJson),
+    credential: admin.credential.cert('serviceAccount.json'),
     storageBucket: "gs://experimentswithsri.appspot.com",
     databaseURL: 'https://experimentswithsri-default-rtdb.firebaseio.com/'
 })
